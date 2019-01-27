1114d9f6726316edc19ff6ed3e8f994a3dc3dae0
dev_auto <mcauto@naver.com>
2019-01-27 23:44:40 +0900

    Modify logger file path

--- logger/logger.go
+++ logger/logger.go
@@ -17,9 +17,9 @@ var (
 
 func init() {
 	// stdFileName standard file log path
-	stdFileName := fmt.Sprintf("/var/log/%s/%s.log", config.Conf.Name, config.Conf.Name)
+	stdFileName := fmt.Sprintf("/tmp/log/%s/%s.log", config.Conf.Name, config.Conf.Name)
 	// errFileName error file log path
-	errFileName := fmt.Sprintf("/var/log/%s/%s_error.log", config.Conf.Name, config.Conf.Name)
+	errFileName := fmt.Sprintf("/tmp/log/%s/%s_error.log", config.Conf.Name, config.Conf.Name)
 
 	stdLumberjackHandle := &lumberjack.Logger{
 		Filename:   stdFileName,
@@ -34,7 +34,6 @@ func init() {
 		MaxBackups: 3,
 		MaxAge:     28, // days
 	}
-
 	Std = distillog.NewStreamLogger("", stdLumberjackHandle)
 	Err = distillog.NewStreamLogger("", errLumberjackHandle)
 }
--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"deotry/config"
+	"deotry/logger"
 	"fmt"
 )
 
 func main() {
 	fmt.Println(config.Conf)
+	logger.Std.Infoln(config.Conf.Name + "is Running")
+	logger.Err.Errorln(config.Conf.Name + "is Running")
 }
